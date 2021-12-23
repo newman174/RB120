d64fa70d0716df4f65be546294c63e77ef753905
newman174 <51267123+newman174@users.noreply.github.com>
2021-12-22 23:46:57 -0500

    easy2 done

---
+++ exercises/easy_2/pets.rb
@@ -0,0 +1,76 @@
+# pets.rb
+
+# My Solution
+
+class Pet
+  attr_accessor :species, :name
+
+  def initialize(species, name)
+    self.species = species
+    self.name = name
+  end
+end
+
+class Owner
+  attr_accessor :name, :adopted_pets
+
+  def initialize(name)
+    self.name = name
+    @adopted_pets = []
+  end
+
+  def adopt(pet)
+    self.adopted_pets.push(pet)
+  end
+
+  def number_of_pets
+    self.adopted_pets.size
+  end
+
+end
+
+class Shelter
+  attr_accessor :adopting_owners
+
+  def initialize
+    self.adopting_owners = []
+  end
+
+  def adopt(owner, pet)
+    self.adopting_owners.push(owner)
+    owner.adopt(pet)
+  end
+
+  def print_adoptions
+    self.adopting_owners.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
+      owner.adopted_pets.each do |pet|
+        puts "a #{pet.species} named #{pet.name}"
+      end
+      puts
+    end
+  end
+end
+
+butterscotch = Pet.new('cat', 'Butterscotch')
+pudding      = Pet.new('cat', 'Pudding')
+darwin       = Pet.new('bearded dragon', 'Darwin')
+kennedy      = Pet.new('dog', 'Kennedy')
+sweetie      = Pet.new('parakeet', 'Sweetie Pie')
+molly        = Pet.new('dog', 'Molly')
+chester      = Pet.new('fish', 'Chester')
+
+phanson = Owner.new('P Hanson')
+bholmes = Owner.new('B Holmes')
+
+shelter = Shelter.new
+shelter.adopt(phanson, butterscotch)
+shelter.adopt(phanson, pudding)
+shelter.adopt(phanson, darwin)
+shelter.adopt(bholmes, kennedy)
+shelter.adopt(bholmes, sweetie)
+shelter.adopt(bholmes, molly)
+shelter.adopt(bholmes, chester)
+shelter.print_adoptions
+puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
+puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
