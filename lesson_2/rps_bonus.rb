4c5a2b528a2477fe196960bd5094979c05232bd1
Michael Newman <newman174@gmail.com>
2021-12-30 12:48:40 -0500

    rps

--- lesson_2/rps_bonus.rb
+++ lesson_2/rps_bonus.rb
@@ -23,10 +23,11 @@ Rule
 =end
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :score
 
   def initialize
     set_name
+    self.score = 0
   end
 end
 
@@ -100,25 +101,21 @@ class Move
   end
 end
 
-class Rule
-  def initialize
-    # not sure what the "state" of a rule object should be
-  end
-end
-
 # Game Orchestration Engine
 class RPSGame
-  @@score = 0 
-
   attr_accessor :human, :computer
 
   def self.score
     @@score
   end
 
+  def self.score=(num)
+    @@score = num
+  end
+
   def initialize
-    # @human = Human.new
-    # @computer = Computer.new
+    @human = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
@@ -135,13 +132,13 @@ class RPSGame
   end
 
   def display_winner
-    if human.move > computer.move
-      puts "#{human.name} won!"
-    elsif human.move < computer.move
-      puts "#{computer.name} won!"
-    else
-      puts "It's a tie!"
-    end
+    # if human.move > computer.move
+    #   puts "#{human.name} won!"
+    # elsif human.move < computer.move
+    #   puts "#{computer.name} won!"
+    # else
+    #   puts "It's a tie!"
+    # end
   end
 
   def play_again?
@@ -160,11 +157,9 @@ class RPSGame
   def play
     display_welcome_message
 
+    round = nil
     loop do
-      human.choose
-      computer.choose
-      display_moves
-      display_winner
+      round = Round.new()
       break unless play_again?
     end
     display_goodbye_message
@@ -172,10 +167,59 @@ class RPSGame
 end
 
 class Round < RPSGame
-  attr_accessor :player_choice, :computer_choice, :result
+  attr_accessor :round_num, :human_move, :computer_move, :winner
 
-  def initialize
+  def initialize(round_num)
+    self.round_num = round_num
+    display_welcome_message
+    human.choose
+    computer.choose
+    record_moves
+    display_moves
+    display_winner
+    break unless play_again?
+  end
+  
+  def play_again?
+    answer = nil
+    loop do
+      puts "Continue to next round? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n."
+    end
+    
+    return false if answer.downcase == 'n'
+    return true if answer.downcase == 'y'
+  end
+  
+  def display_welcome_message
+    puts "Round #{round_num}:"
+  end
+  
+  def record_moves
+    self.human_move = human.move
+    self.computer_move = computer.move
+  end
 
+  def winner
+    case
+    when human.move > computer.move
+      human
+    when human.move < computer.move
+      computer
+    else
+      :tie
+    end
+  end
+  
+  def display_winner    
+    case winner
+    when :tie
+      puts "It's a tie!"
+    else
+      puts "#{winner.name} won!"
+    end
   end
 end
 
