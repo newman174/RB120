2e614731fbc9130e27c8058d2acd47d20f05dd4c
Michael Newman <newman174@gmail.com>
2022-01-05 12:48:42 -0500

    rename detect_winner to winning_marker

--- lesson_5/4_oo_ttt_improvements.rb
+++ lesson_5/4_oo_ttt_improvements.rb
@@ -27,7 +27,7 @@ class Board
   end
 
   def someone_won?
-    !!detect_winner
+    !!winning_marker
   end
 
   def count_human_marker(squares)
@@ -39,7 +39,7 @@ class Board
   end
 
   # return winning marker or nil
-  def detect_winner
+  def winning_marker
     WINNING_LINES.each do |line|
       if count_human_marker(@squares.values_at(*line)) == 3
         return TTTGame::HUMAN_MARKER
@@ -143,7 +143,7 @@ class TTTGame
   def display_result
     clear_screen_and_display_board
 
-    case board.detect_winner
+    case board.winning_marker
     when human.marker
       puts "You won!"
     when computer.marker
