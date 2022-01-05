5d019c2b43fec20c074e1456b46a66737fb40bda
Michael Newman <newman174@gmail.com>
2022-01-05 13:23:16 -0500

    refactor winning marker

--- lesson_5/4_oo_ttt_improvements.rb
+++ lesson_5/4_oo_ttt_improvements.rb
@@ -25,31 +25,22 @@ class Board
   def someone_won?
     !!winning_marker
   end
-
-  def count_human_marker(squares)
-    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
-  end
-
-  def count_computer_marker(squares)
-    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
-  end
-
+  
   # return winning marker or nil
   def winning_marker
     WINNING_LINES.each do |line|
-      if count_human_marker(@squares.values_at(*line)) == 3
-        return TTTGame::HUMAN_MARKER
-      elsif count_computer_marker(@squares.values_at(*line)) == 3
-        return TTTGame::COMPUTER_MARKER
+      squares = @squares.values_at(*line)
+      if three_identical_markers?(squares)
+        return squares.first.marker
       end
     end
     nil
   end
-
+  
   def reset
     (1..9).each { |key| @squares[key] = Square.new }
   end
-
+  
   def draw
     puts "     |     |"
     puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
@@ -62,6 +53,13 @@ class Board
     puts "     |     |"
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
     puts "     |     |"
+  end
+
+  private
+  def three_identical_markers?(squares)
+    markers = squares.select(&:marked?).collect(&:marker)
+    return false if markers.size != 3
+    markers.min == markers.max
   end
 end
 
@@ -80,6 +78,10 @@ class Square
 
   def unmarked?
     marker == INITIAL_MARKER
+  end
+
+  def marked
+    marker != INITIAL_MARKER
   end
 end
 
