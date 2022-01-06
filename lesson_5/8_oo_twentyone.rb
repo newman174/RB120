ef90248892e2e47021850703bdc8b991ac7e0778
Michael Newman <newman174@gmail.com>
2022-01-06 15:43:57 -0500

    twentyone rb dev checkpoint

--- lesson_5/8_oo_twentyone.rb
+++ lesson_5/8_oo_twentyone.rb
@@ -39,78 +39,327 @@ Game
 
 =end
 
-class Player
-  def initialize
-    # what would the "data" or "states" of a Player object entail?
-    # maybe cards? a name?
-  end
+require 'pry'
 
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
-    # definitely looks like we need to know about "cards" to produce some total
-  end
-end
-
-class Dealer
-  def initialize
-    # seems like very similar to Player... do we even need this?
-  end
-
-  def deal
-    #  does the dealer or the deck deal?
-  end
-
-  def hit
-  end
-
-  def stay
-  end
-
-  def busted?
-  end
-
-  def total
+module Prompt
+  PROMPT_SYMBOL = ">> "
+  def prompt(msg, blank: false, blank_lines: 0)
+    prefix = blank ? '   ' : PROMPT_SYMBOL
+    puts "#{prefix}#{msg}"
+    blank_lines.times { puts }
   end
 end
 
 class Participant
-  # what goes in here? all redundant behaviors from Player and Dealer?
+  include Prompt
+
+  attr_accessor :name, :hand
+
+  def initialize(name)
+    self.name = name
+    self.hand = []
+  end
+
+  def hit(deck)
+    hand << deck.cards.pop
+  end
+
+  def deal(deck)
+    2.times { hit(deck) }
+  end
+
+  def total
+    total = 0
+    hand.each { |card| total += card.value }
+    total
+  end
+
+  def discreet_total
+    if hand.all? { |card| card.visible }
+      total
+    else
+      '??'
+    end
+  end
+
+  def display_total
+    prompt "#{name}'s current total: #{total}", blank_lines: 1
+  end
+
+  def display_hand
+    prompt "#{name} has #{discreet_total} points:"
+    hand.each do |card|
+      prompt card, blank: true
+    end
+    puts
+  end
+
+  def to_s
+    name
+  end
+
+  def reset_hand
+    self.hand = []
+  end
+end
+
+class Player < Participant
+  def hit?
+    choice = nil
+    loop do
+      prompt "Would you like to hit or stay? Enter h or s."
+      choice = gets.chomp.downcase
+      break if ['h', 's'].include?(choice)
+      prompt "Sorry that's not a valid choice. Please enter h or s."
+    end
+    choice == 'h'
+  end
+end
+
+class Dealer < Participant
+  def initialize(name)
+    super
+  end
+
+  def deal(deck)
+    super
+    hand.last.visible = false
+  end
+
+  def hit?
+    total < Game::DEALER_STAY_AT
+  end
 end
 
 class Deck
-  def initialize
-    # obviously, we need some data structure to keep track of cards
-    # array, hash, something else?
-  end
+  CARD_NAMES = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+  SUITS = %w(♤ ♡ ♧ ♢)
 
-  def deal
-    # does the dealer or deck deal?
+  attr_accessor :cards
+
+  def initialize
+    self.cards = []
+    CARD_NAMES.each do |name|
+      SUITS.each do |suit|
+        cards << Card.new(name, suit)
+      end
+    end
+    cards.shuffle!
   end
 end
 
 class Card
-  def initialize
+  VALUES = { 'A'   =>  1,
+             '2'     =>  2,
+             '3'     =>  3,
+             '4'     =>  4,
+             '5'     =>  5,
+             '6'     =>  6,
+             '7'     =>  7,
+             '8'     =>  8,
+             '9'     =>  9,
+             '10'    => 10,
+             'J'  => 10,
+             'Q' => 10,
+             'K'  => 10 }
+
+  attr_accessor :name, :value, :suit, :visible
+
+  def initialize(name, suit)
     # what are the states of a card?
+    self.name = name
+    self.suit = suit
+    self.value = VALUES[name]
+    self.visible = true
+  end
+
+  def to_s
+    if visible
+      "[#{format("%2s", name)} #{suit} ]"
+    else
+      "[  ?  ]"
+    end
+  end
+
+  def draw
+  end
+end
+
+module GameDisplay
+  
+  class Display
+    attr_accessor :frames
+
+    def initialize
+      self.frames = []
+    end
+
+    def show
+      frames.each(&:print)
+    end
+
+  class Frame
+    attr_accessor :lines
+
+    def initialize
+      self.lines = []
+    end
+
+    def width
+      lines.map { |line| line.length }.max
+    end
+
+    def print
+
+    end
   end
 end
 
 class Game
-  def start
-    # what's the sequence of steps to execute the game play?
-    deal_cards
-    show_initial_cards
+  include Prompt
+
+  MAX_POINTS = 21
+  DEALER_STAY_AT = 17
+
+  attr_accessor :deck, :player, :dealer, :participants, :winner
+
+  def initialize
+    self.deck = Deck.new
+    self.player = Player.new("Mike")
+    self.dealer = Dealer.new("CPU Dealer")
+    self.participants = [player, dealer]
+  end
+
+  def deal_cards
+    participants.each do |part|
+      part.deal(deck)
+    end
+  end
+
+  def display_cards
+    participants.each { |part| part.display_hand }
+  end
+
+  def busted?(participant)
+    participant.total > MAX_POINTS
+  end
+
+  def participant_turn(participant)
+    loop do
+      clear
+      prompt "#{participant}'s Turn", blank_lines: 1
+      display_cards
+      if participant.hit?
+        participant.hit(deck)
+        clear
+        prompt "#{participant} hits and is dealt a #{participant.hand.last}"
+        display_cards
+
+        if busted?(participant)
+          prompt "#{participant} busted!", blank_lines: 1
+          break
+        end
+      else
+        prompt "#{participant} stays.", blank_lines: 1
+        break
+      end
+    end
+  end
+
+  def player_turn
+    participant_turn(player)
+  end
+
+  def dealer_turn
+    participant_turn(dealer)
+  end
+
+  def determine_winner
+    if busted?(player)
+      dealer
+    elsif busted?(dealer)
+      player
+    elsif player.total == dealer.total
+      :tie
+    else
+      player.total > dealer.total ? player : dealer
+    end
+  end
+
+  def show_result
+    participants.each { |part| part.display_total }
+  
+    winner = determine_winner
+    if winner == :tie
+      prompt "It's a tie!"
+    else
+      prompt "#{winner} is the winner!"
+    end
+  end
+
+  def take_turns
     player_turn
+    enter_to_continue
+    return if busted?(player)
     dealer_turn
-    show_result
+  end
+
+  def display_welcome_screen
+    clear
+    prompt "Welcome to Twenty One!\n"
+    prompt "[RULES]\n"
+    enter_to_continue
+    clear
+  end
+
+  def enter_to_continue
+    prompt "Press enter to continue."
+    gets
+  end
+
+  def reset
+    self.deck = Deck.new
+    participants.each { |part| part.reset_hand}
+    prompt "New Game!"
+    enter_to_continue
+    clear
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      prompt "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include? answer
+      prompt "Sorry, must be y or n"
+    end
+
+    answer == 'y'
+  end
+
+  def display_goodbye_message
+    prompt "Thanks for playing Twenty One. Goodbye!"
+  end
+
+  def clear
+    system 'clear'
+  end
+
+  def play
+    display_welcome_screen
+    loop do
+      deal_cards
+      display_cards
+      take_turns
+      show_result
+      break unless play_again?
+      reset
+    end
+    clear
+    display_goodbye_message
   end
 end
 
-Game.new.start
+game = Game.new
+game.play
+# binding.pry
